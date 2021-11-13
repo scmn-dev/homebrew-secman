9a4481b259797267368182574d111bfbc4b19753
abdfnx <abdfnabd@gmail.com>
2021-11-13 10:02:06 +0000

    secman v6.0.61

--- Formula/secman.rb
+++ Formula/secman.rb
@@ -2,7 +2,7 @@ class Secman < Formula
   desc "The Password Manager of your dreams"
   homepage "https://secman.dev/docs/cli"
   url "https://cli-files.secman.dev/secman-v6.0.61/secman-v6.0.61.tar.gz"
-  sha256 "56c031f5b19182d2ca7b48e8def49b8832f5e54d9240ccb6ed2b95d9b5cc98b3"
+  sha256 "dbb53beaf763bf30c9d4909bab68e2f14c1c63e5300f44de8d1ab6fe11c96e8c"
   depends_on "scmn-dev/secman/sm-node" => "16.13.0"
 
   def install
