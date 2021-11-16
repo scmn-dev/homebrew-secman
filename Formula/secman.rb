ea6284701a9a85046fe6f40e4044294dff1c14d1
abdfnx <abdfnabd@gmail.com>
2021-11-16 11:18:19 +0000

    secman v6.0.71

--- Formula/secman.rb
+++ Formula/secman.rb
@@ -3,8 +3,8 @@ class Secman < Formula
   homepage "https://secman.dev/docs/cli"
   url "https://cli-files.secman.dev/secman-v6.0.71/secman-v6.0.71.tar.gz"
   sha256 "2a9c03df9b444e9c110b640c2e22ef4db147623cb728f7b2c68f2bc488d0e230"
-  depends_on "scmn-dev/secman/sm-node" => "16.13.0"
   license "MIT"
+  depends_on "scmn-dev/secman/sm-node" => "16.13.0"
 
   livecheck do
     url :homepage
