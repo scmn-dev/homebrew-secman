837fc1f1a6d013b8cd5ea1cbf2fef686fa97c9d8
abdfnx <abdfnabd@gmail.com>
2021-11-16 11:15:27 +0000

    secman v6.0.71

--- Formula/secman.rb
+++ Formula/secman.rb
@@ -1,16 +1,23 @@
 class Secman < Formula
-  desc "👊 Human-friendly and amazing secrets manager."
+  desc "👊 Human-friendly and amazing secrets manager"
   homepage "https://secman.dev/docs/cli"
   url "https://cli-files.secman.dev/secman-v6.0.71/secman-v6.0.71.tar.gz"
   sha256 "2a9c03df9b444e9c110b640c2e22ef4db147623cb728f7b2c68f2bc488d0e230"
   depends_on "scmn-dev/secman/sm-node" => "16.13.0"
+  license "MIT"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?aamath[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   def install
     inreplace "bin/secman", /^CLIENT_HOME=/, "export SECMAN_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     inreplace "bin/secman", "\"$DIR/node\"", Formula["sm-node"].opt_bin/"node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/secman"
-    system("env SM_PROVIDER=brew")
+    # system("env SM_PROVIDER=brew")
+    ENV["SM_PROVIDER"] = "brew"
   end
 
   test do
