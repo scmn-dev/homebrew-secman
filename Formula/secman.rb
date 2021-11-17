c39e3d71fb15539b1b30c7ffe7e980e2a0e1be19
abdfnx <abdfnabd@gmail.com>
2021-11-17 11:47:04 +0000

    secman v6.0.71

--- Formula/secman.rb
+++ Formula/secman.rb
@@ -4,7 +4,7 @@ class Secman < Formula
   url "https://cli-files.secman.dev/secman-v6.0.71/secman-v6.0.71.tar.gz"
   sha256 "2a9c03df9b444e9c110b640c2e22ef4db147623cb728f7b2c68f2bc488d0e230"
   license "MIT"
-  head "https://github.com/scmn-dev/secman", branch: "main"
+  head "https://github.com/scmn-dev/secman.git", branch: "main"
 
   livecheck do
     url "https://github.com/scmn-dev/secman/releases/latest"
