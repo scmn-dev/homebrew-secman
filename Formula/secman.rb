be237bb8c860b4a8a976950fafad6c7dcac7b233
abdfnx <abdfnabd@gmail.com>
2021-11-17 11:41:08 +0000

    secman v6.0.71

--- Formula/secman.rb
+++ Formula/secman.rb
@@ -4,26 +4,26 @@ class Secman < Formula
   url "https://cli-files.secman.dev/secman-v6.0.71/secman-v6.0.71.tar.gz"
   sha256 "2a9c03df9b444e9c110b640c2e22ef4db147623cb728f7b2c68f2bc488d0e230"
   license "MIT"
+  head "https://github.com/scmn-dev/secman", branch: "main"
 
   livecheck do
     url "https://github.com/scmn-dev/secman/releases/latest"
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
+  depends_on "node"
+
   def install
     inreplace "bin/secman", /^CLIENT_HOME=/, "export SECMAN_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/secman", "\"$DIR/node\"", Formula["node"].opt_bin/"node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/secman"
-
-    # this variable is used for `update` command
-    # https://github.com/scmn-dev/secman/blob/main/src/commands/update.ts#L48
-    ENV["SM_PROVIDER"] = "brew"
   end
 
   test do
     system bin/"secman", "init"
     system bin/"secman", "crypto", "HOMEBREW"
-    assert_match "secman v6.0.71 (2021-11-13)\nhttps://github.com/scmn-dev/secman/releases/tag/v6.0.71",
+    assert_match "secman v#{version} (2021-11-13)\nhttps://github.com/scmn-dev/secman/releases/tag/v#{version}",
     shell_output("#{bin}/secman --version")
   end
 end
